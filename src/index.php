34c526a2c00714551de0d1ec570705c9847965e5
Thomas Hofmann <thomas.hofmann@h-da.de>
2023-10-20 14:19:29 +0200

    New title

--- src/index.php
+++ src/index.php
@@ -21,7 +21,7 @@ echo <<<HTML
 <head>
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Start</title>
+    <title>EWA Repository</title>
     <link rel="stylesheet" href="index.css">
 </head>
 <body>
